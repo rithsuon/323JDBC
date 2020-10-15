fde4bae8dd89d3a39ec25034a51a1a673d30299c
rithsuon <rithrangsey.suon@student.csulb.edu>
2020-10-14 23:24:42 -0700

    testing push

--- src/main/java/com/mycompany/jdbcproj/NewClass.java
+++ src/main/java/com/mycompany/jdbcproj/NewClass.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.jdbcproj;
 
 /**
  *
  * @author Rith
  */
 public class NewClass {
-    
+    public static void main(String args[]) {
+        System.out.println("hello");
+    }
 }
